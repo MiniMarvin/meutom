2c5d602ddd99ffbe334329e4e8f419e1e0473cd4
Caio Gomes <caiogomes34@gmail.com>
2025-10-20 16:54:13 -0300

    setup the layout of how the heading of the page paints

--- meutom-web/src/routes/+page.svelte
+++ meutom-web/src/routes/+page.svelte
@@ -1,12 +1,77 @@
 <script>
 	import TextMarkerHightlight from 'src/lib/text-marker-hightlight.svelte';
+	import background from '$lib/assets/background.png?enhanced';
 
 	TextMarkerHightlight;
 </script>
 
-# meutom Digitalize e arrume seu guarda roupas com as peças que você tem na sua casa! Monte looks
-com as peças dos seu guarda roupa, acompanhe o uso das suas peças, e atualize sua imagem de maneira
-consciente com brechós.
-<TextMarkerHightlight markerColor={{ r: 255, g: 176, b: 209 }}>
-	Visualize como como uma peça nova se encaixa no seu guarda roupas
-</TextMarkerHightlight>, antes de comprar!
+<section class="head">
+	<enhanced:img
+		src={background}
+		alt="duas pessoas com seus looks olhando para frente"
+		width="100%"
+		class="top"
+	></enhanced:img>
+	<div class="center">
+		<div class="centered">
+			<h1>Digitalize seu guarda roupas!</h1>
+			<h3>
+				Monte looks com as peças que você tem e encontre roupas de brechós com facilidade, <TextMarkerHightlight
+					markerColor={{ r: 255, g: 176, b: 209 }}
+				>
+					compre suas peças com o novo look em mente!
+				</TextMarkerHightlight>
+			</h3>
+		</div>
+	</div>
+</section>
+
+<style>
+	h1,
+	h3 {
+		color: white;
+	}
+
+	h1 {
+		font-size: calc(max(4vw, 36px));
+		text-align: center;
+	}
+
+	h3 {
+		padding: 0px 0px;
+		margin: 14px 0px;
+		text-align: center;
+	}
+
+	.top {
+		top: 0;
+		max-width: 100vw;
+		max-height: 100%;
+		width: 100%;
+		object-fit: cover;
+		position: absolute;
+		z-index: -1;
+		filter: blur(2px);
+	}
+
+	.center {
+		padding: 12px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.centered {
+		max-width: 800px;
+		padding-bottom: 80px;
+	}
+
+	.head {
+		/* max-height: 50vh; */
+		overflow: hidden;
+		position: relative;
+		background-color: #333;
+		z-index: -2;
+	}
+</style>
--- meutom-web/vite.config.ts
+++ meutom-web/vite.config.ts
@@ -2,9 +2,11 @@ import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 
 export default defineConfig({
 	plugins: [
+		enhancedImages(),
 		sveltekit(),
 		devtoolsJson(),
 		paraglideVitePlugin({
