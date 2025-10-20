d1e190d9a536980594e5bac09239c421f98026ea
Caio Gomes <caiogomes34@gmail.com>
2025-10-20 15:21:38 -0300

    setup minimum integration for mui and initial text

---
+++ meutom-web/src/lib/text-marker-hightlight.svelte
@@ -0,0 +1,41 @@
+<!-- Reference: https://stackoverflow.com/questions/33451683/pen-highlighter-effect-in-css -->
+<script lang="ts">
+	import type { Color } from 'src/types';
+	import type { Snippet } from 'svelte';
+
+	const props: {
+		markerColor: Color;
+		children?: Snippet;
+	} & HTMLSpanElement = $props();
+</script>
+
+<span
+	style={`font-weight: bolder;
+		background:
+			linear-gradient(
+				104deg,
+				rgba(${props.markerColor.r}, ${props.markerColor.g}, ${props.markerColor.b}, 0) 0.9%,
+				rgba(${props.markerColor.r}, ${props.markerColor.g}, ${props.markerColor.b}, 1.25) 2.4%,
+				rgba(${props.markerColor.r}, ${props.markerColor.g}, ${props.markerColor.b}, 0.5) 5.8%,
+				rgba(${props.markerColor.r}, ${props.markerColor.g}, ${props.markerColor.b}, 0.1) 93%,
+				rgba(${props.markerColor.r}, ${props.markerColor.g}, ${props.markerColor.b}, 0.7) 96%,
+				rgba(${props.markerColor.r}, ${props.markerColor.g}, ${props.markerColor.b}, 0) 98%
+			),
+			linear-gradient(
+				183deg,
+				rgba(${props.markerColor.r}, ${props.markerColor.g}, ${props.markerColor.b}, 0) 0%,
+				rgba(${props.markerColor.r}, ${props.markerColor.g}, ${props.markerColor.b}, 0.3) 7.9%,
+				rgba(${props.markerColor.r}, ${props.markerColor.g}, ${props.markerColor.b}, 0) 15%
+			);
+		padding: 0.3em 10px;
+		box-decoration-break: clone;
+		-webkit-box-decoration-break: clone;
+		margin: 0;
+		border-radius: 7.5px;
+		text-shadow:
+			-12px 12px 9.8px rgba(${props.markerColor.r}, ${props.markerColor.g}, ${props.markerColor.b}, 0.7),
+			21px -18.1px 7.3px rgba(255, 255, 255, 1),
+			-18.1px -27.3px 30px rgba(255, 255, 255, 1); ${props.style}`}
+>
+	{@render props.children?.()}
+</span>
--- meutom-web/src/routes/+page.svelte
+++ meutom-web/src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import TextMarkerHightlight from 'src/lib/text-marker-hightlight.svelte';
+
+	TextMarkerHightlight;
+</script>
+
+# meutom Digitalize e arrume seu guarda roupas com as peças que você tem na sua casa! Monte looks
+com as peças dos seu guarda roupa, acompanhe o uso das suas peças, e atualize sua imagem de maneira
+consciente com brechós.
+<TextMarkerHightlight markerColor={{ r: 255, g: 176, b: 209 }}>
+	Visualize como como uma peça nova se encaixa no seu guarda roupas
+</TextMarkerHightlight>, antes de comprar!
